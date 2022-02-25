99482a22011115ddbc91b4e52bf79642b23cbc55
Loubna Ben Allal <loubna.ben-allal8@etu.univ-lorraine.fr>
2022-02-25 02:11:57 +0100

    make the file script

--- source/node_embedding.py
+++ source/node_embedding.py
@@ -2,30 +2,32 @@ import os
 import gzip
 import pickle
 from tqdm import tqdm
-
 import networkx as nx
 from nodevectors import Node2Vec
 
 
-EMBEDDING_FILENAME = './node_embeddings.emb'
+def create_node_embeddings(G, nodes, embedding_path, emb_size=128):
+    model = Node2Vec(n_components=emb_size, 
+                walklen=40,
+                epochs=30,
+                threads=os.cpu_count())  
 
-# Create a graph
-G = nx.read_edgelist('edgelist.txt', delimiter=',', create_using=nx.Graph(), nodetype=int)
-nodes = list(G.nodes())
-n = G.number_of_nodes()
-m = G.number_of_edges()
+    model.fit(G)
 
-model = Node2Vec(n_components=128, 
-               walklen=40,
-               epochs=30,
-               threads=os.cpu_count())  
+    node_embeddings = dict()
+    for node in tqdm(nodes):
+        node_embeddings[node] = model.predict(node)
+        
+    file = gzip.GzipFile(EMBEDDING_FILENAME, 'wb')
+    file.write(pickle.dumps(node_embeddings))
+    file.close()
 
-model.fit(G)
+if __name__ =="__main__":
+    EMBEDDING_FILENAME = 'embeddings/node_embeddings.emb'
 
-node_embeddings = dict()
-for node in tqdm(nodes):
-    node_embeddings[node] = model.predict(node)
-    
-file = gzip.GzipFile(EMBEDDING_FILENAME, 'wb')
-file.write(pickle.dumps(node_embeddings))
-file.close()
+    G = nx.read_edgelist('data/edgelist.txt', delimiter=',', create_using=nx.Graph(), nodetype=int)
+    nodes = list(G.nodes())
+    n = G.number_of_nodes()
+    m = G.number_of_edges()
+
+    create_node_embeddings(G, nodes, EMBEDDING_FILENAME)
