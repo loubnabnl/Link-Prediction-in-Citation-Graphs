1f2cd18ab36239923c6baadbe963e439659c2e3d
Loubna Ben Allal <loubna.ben-allal8@etu.univ-lorraine.fr>
2022-02-25 02:43:08 +0100

    add code for dimension reduction

--- source/abstract_scibert_embedding.py
+++ source/abstract_scibert_embedding.py
@@ -2,9 +2,52 @@ import networkx as nx
 import torch
 from transformers import AutoTokenizer, AutoModel
 from tqdm import tqdm
+import numpy as np
 import pickle
 import gzip
 import argparse
+
+from sklearn.decomposition import PCA
+
+def dim_reduction(text2vec, first_size = 250, final_size=64):
+  """method to reduce dimension of abstract embeddings in text2vec to the size: final_size
+  source code: https://github.com/vyraun/Half-Size/blob/master/algo.py"""
+  pca_embeddings = {}
+
+  # PCA to get Top Components
+  # first_size must be smaller than the mebdding size
+  pca =  PCA(n_components = first_size)
+  X = text2vec - np.mean(text2vec)
+  X_fit = pca.fit_transform(X)
+  U1 = pca.components_
+  z = []
+
+  # Removing Projections on Top Components
+  for i, x in enumerate(X):
+    for u in U1[0:7]:        
+            x = x - np.dot(u.transpose(),x) * u 
+    z.append(x)
+  z = np.asarray(z)
+
+  # PCA Dim Reduction
+  pca =  PCA(n_components = final_size)
+  X = z - np.mean(z)
+  X = pca.fit_transform(X)
+
+  # PCA to do Post-Processing Again
+  pca =  PCA(n_components = final_size)
+  X = X - np.mean(X)
+  X = pca.fit_transform(X)
+  Ufit = pca.components_
+
+  X = X - np.mean(X)
+
+  # Place embeddings in dictionnary
+  text2vec_small = dict()
+  keys = list(text2vec.keys())
+  for i in range(len(keys)):
+    text2vec_small[keys[i]] = X[i]
+  return text2vec_small
 
 def create_scibert_embeddings(args):
   """create embeddings for paper abstracts using pretrained SciBERT"""
@@ -47,6 +90,8 @@ def create_scibert_embeddings(args):
     token_embeddings = token_embeddings.squeeze(0)
     
     text2vec[node_id] = token_embeddings
+  if args.dim_reduction:
+        text2vec = dim_reduction(text2vec, final_size = args.reduction_size)
   print("----- Saving the Embeddings -----")
   file = gzip.GzipFile(args.path_save, 'wb')
   file.write(pickle.dumps(text2vec))
@@ -60,6 +105,10 @@ if __name__ == '__main__':
         help="Path to the graph edges text file")
     parser.add_argument("-pa", "--abstracts", type=str, default="data/abstracts.txt", 
         help="Path to the abstracts text file")
+    parser.add_argument("-pca", "--dim_reduction", type=bool, default=False, 
+        help="Argument scpecifying whether to keep the embedding size 768 of scibert or reduce it")
+    parser.add_argument("-dr", "--dim_reduction", type=int, default=64, 
+        help="embedding size after dimension reduction if applied")
     parser.add_argument("-ps", "--path_save", type=str, default="embeddings/abstracts_scibert_embeddings.emb",
                         help="Path to save the abstract embeddings file")
 
