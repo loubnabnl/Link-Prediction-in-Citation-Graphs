4999dc4fdf57ebe94d089c78497bd2f980321c78
Loubna Ben Allal <loubna.ben-allal8@etu.univ-lorraine.fr>
2022-02-27 21:20:19 +0100

    restructure code

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import csv
 from xgboost import XGBClassifier
 import argparse
 from utils import load_features, sample_negative_links, get_training_graph, extract_features, return_metrics
-from models import train_MLP
+from models.MLP import train_MLP
 
 
 def main(args):
@@ -27,26 +27,27 @@ def main(args):
     text2vec = load_features(args.abstract_path)
     aut2vec = load_features(args.authors_path)
     nodes2vec = load_features(args.nodes_path)
-    
+
+    print('Creating positive and negative samples...')
     pos_samples_train, neg_samples_train, pos_samples_dev, neg_samples_dev = sample_negative_links(G, 
-                                                                    args.neg_ratio, test_node_pairs)
-    
-    graph = get_training_graph(G, pos_samples_dev)
-    graph_dicts = dict()
-    graph_dicts["clustering_coeff"] = nx.algorithms.cluster.clustering(graph)
-    graph_dicts["eigenvector"] = nx.algorithms.centrality.eigenvector_centrality(graph)
-    
+                                                                    args.neg_ratio)
     #training and validation data
     train_samples = pos_samples_train + neg_samples_train
     train_labels = [1 for x in pos_samples_train] + [0 for x in neg_samples_train]
     dev_samples = pos_samples_dev + neg_samples_dev
     dev_labels = [1 for x in pos_samples_dev] + [0 for x in neg_samples_dev]
     
-    adjacency_author = np.load(args.authors_adj)
+    graph = get_training_graph(G, pos_samples_dev)
         
     print('Generating training and validation features...')  
-    X_train = extract_features(graph, authors, nodes2vec, text2vec,aut2vec, train_samples, graph_dicts, path_adj=args.authors_adj)
-    X_dev = extract_features(graph, authors, nodes2vec, text2vec,aut2vec, dev_samples, graph_dicts, path_adj=args.authors_adj)
+    graph_dicts = dict()
+    graph_dicts["clustering_coeff"] = nx.algorithms.cluster.clustering(graph)
+    graph_dicts["eigenvector"] = nx.algorithms.centrality.eigenvector_centrality(graph)
+    
+    adjacency_author = np.load(args.authors_adj)
+
+    X_train = extract_features(graph, authors, nodes2vec, text2vec, aut2vec, train_samples, graph_dicts, path_adj=args.authors_adj)
+    X_dev = extract_features(graph, authors, nodes2vec, text2vec, aut2vec, dev_samples, graph_dicts, path_adj=args.authors_adj)
     
     #classification
     print(f'Training Classifier {args.model} ...')
@@ -58,7 +59,7 @@ def main(args):
         clf = train_MLP(X_train, train_labels, X_dev, dev_labels)
     
     else:
-        raise ValueError('Invalid classifcation model name')
+        raise ValueError('Invalid classification model name')
     
     print("Test phase...")
     test_node_pairs = list()
@@ -67,10 +68,8 @@ def main(args):
             t = line.split(',')
             test_node_pairs.append((int(t[0]), int(t[1])))
     
-    if not(X_test):
-        X_test = extract_features(graph, authors, nodes2vec, text2vec, aut2vec, test_node_pairs, graph_dicts, path_adj=args.authors_adj)
-        
-        y_pred = clf.predict_proba(X_test)[:,1]
+    X_test = extract_features(graph, authors, nodes2vec, text2vec, aut2vec, test_node_pairs, graph_dicts, path_adj=args.authors_adj)
+    y_pred = clf.predict_proba(X_test)[:,1]
         
     predictions = zip(range(len(y_pred)), y_pred)
     with open(f"submission.csv","w") as pred:
@@ -83,23 +82,23 @@ def main(args):
 if __name__ =="__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-pg", "--path_graph", type=str, default="data/edgelist.txt", 
+    parser.add_argument("--path_graph", type=str, default="data/edgelist.txt", 
         help="Path to the graph edges text file")
-    parser.add_argument("-a", "--authors", type=str, default="data/authors.txt", 
+    parser.add_argument("--authors", type=str, default="data/authors.txt", 
         help="Path to the author  text file")
-    parser.add_argument("-pg", "--path_test", type=str, default="data/test.txt", 
+    parser.add_argument("--path_test", type=str, default="data/test.txt", 
         help="Path to the test file")
-    parser.add_argument("-ab", "--abstract_path", type=str, default="embeddings/abstract_embeddings.emb",
-                        help="Path to the abstract text file")
-    parser.add_argument("-pn", "--nodes_path", type=str, default="embeddings/node_embeddings.emb", 
-        help="Path to the node embeddings file")
-    parser.add_argument("-pa", "--authors_path", type=str, default="embeddings/authors_embeddings.emb", 
-        help="Path to the author embeddings file")
-    parser.add_argument("-adj", "--authors_adj", type=str, default="embeddings/adjacencyfinal.npz", 
+    parser.add_argument("--abstract_path", type=str, default="embeddings/abstract_embeddings.emb",
+                        help="Path to the abstract embeddings")
+    parser.add_argument("--nodes_path", type=str, default="embeddings/node_embeddings.emb", 
+        help="Path to the node embeddings")
+    parser.add_argument("--authors_path", type=str, default="embeddings/authors_embeddings.emb", 
+        help="Path to the author embeddings")
+    parser.add_argument("--authors_adj", type=str, default="embeddings/adjacencyfinal.npz", 
         help="Path to the author graph adjacency matrix")
-    parser.add_argument("-nr", "--neg_ratio", type=int, default=1, 
+    parser.add_argument("--neg_ratio", type=int, default=1, 
         help="ratio of negative samples")
-    parser.add_argument("-m", "--model", type=str, default="xgboost", 
-        help="Model to compute the predictions")
+    parser.add_argument("--model", type=str, default="xgboost", 
+        help="classification model")
 
     main(parser.parse_args())    
